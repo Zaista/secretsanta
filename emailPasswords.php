5387657ad3d5f1ddf2f9f09dfa4ebcbe8fac4660
Joca <ilicjovan89@gmail.com>
2020-11-28 16:51:58 +0100

    Fancify email

--- emailPasswords.php
+++ emailPasswords.php
@@ -51,17 +51,42 @@
         global $users;
         foreach ($users as $user => $user_data) {
             if ($user_data["email"] != null) {
+
                 $email_subject = "How to access your super neat Secret Santa place";
-                $email_text = '
-Secret Santa super secret info:
-    secret_santa_username: ' . $user . '
-    santa_super_password: ' . $user_data["password"] . '
-    secret_not_so_secret_address: ' . $user_data["address"] . '
-    super_public_link: https://secretsanta.jovanilic.com
 
-Merry shopping und Alles Gute zum Gechristmas :)';
+                $email_headers = "From: secretsanta@jovanilic.com\r\n";
+                $email_headers .= "MIME-Version: 1.0\r\n";
+                $email_headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-                $email_headers = "From: secretsanta@jovanilic.com";
+                $email_text = '<html><head><style>' . 
+                'table {' .
+                    'max-width: 730px; ' .
+                    'margin: 25px 0;' .
+                    'box-shadow: 0 0 20px rgba(0, 0, 0, 0.15); }' .
+                'table, th, td {' .
+                    'border-collapse: collapse;' .
+                    'font-size: 0.9em;' .
+                    'font-family: sans-serif;' .
+                    'min-width: 400px; }' .
+                '.padds {' .
+                    'padding: 12px 15px; }' .
+                'th {' .
+                    'background-color: #fc0000;' .
+                    'color: #ffffff;' .
+                    'text-align: left; }' .
+                'tr {' .
+                    'border-bottom: 1px solid #dddddd; }' .
+                'tr:last-of-type {' .
+                    'border-bottom: 2px solid #009879; }' .
+                '</style></head><body>';
+                $email_text .= '<table><tr><td colspan="2" style="overflow: hidden; max-height: 254px"><img src="https://secretsanta.jovanilic.com/resources/images/santa.jpg" style="width: calc(100% + 24px); position: relative; left: -12px"></td></tr>';
+                $email_text .= '<tr><th class="padds">User:</th><td class="padds">' . $user . '</td></tr>';
+                $email_text .= '<tr><th class="padds">Password:</th><td class="padds">' . $user_data["password"] . '</td></tr>';
+                $email_text .= '<tr><th class="padds">Address:</th><td class="padds">' . $user_data["address"] . '</td></tr>';
+                $email_text .= '<tr><td colspan="2" class="padds" style="text-align: center">Link to <a href="https://secretsanta.jovanilic.com" target="_blank">SecretSanta</a> webiste</td></tr>';
+                $email_text .= '</table>';
+                $email_text .= '<p>Merry shopping und Alles Gute zum Gechristmas :)</p>';
+                $email_text .= '</body></html>';
 
                 // use wordwrap() if lines are longer than 70 characters
                 $email_text = wordwrap($email_text, 70);
