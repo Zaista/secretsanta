4adcaabf4ff2b66426e70d570ff286a09974acaa
Joca <ilicjovan89@gmail.com>
2021-01-05 11:03:53 +0100

    Minor fixes

--- private/connect.php
+++ private/connect.php
@@ -1,18 +1,17 @@
 <?php
 
     function get_config($config) {
-        // load configuration file
-        $xml = simplexml_load_file($config) or die("Error: Cannot load configuration file");
+
+        $xml = simplexml_load_file($config);
         return $xml;
     }
 
     function connect($xml) {
         
         global $output;
-
-        // create connection
+        
         $mysqli = new mysqli($xml->database->hostname, $xml->database->username, $xml->database->password, $xml->database->database);
-        // check connection
+        
         if ($mysqli->connect_error) {
             $output->error = "Connection failed: " . $mysqli->connect_error;
             echo json_encode($output);
