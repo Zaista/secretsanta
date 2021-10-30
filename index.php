4b31fd75f9ef4fa08fad37cbb7cddb8e522504d8
Joca <ilicjovan89@gmail.com>
2021-10-30 21:42:43 +0200

    fix: cleanup and yaml fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 
     //  Load the application code.
     /** @var Slim\App $app */
@@ -10,16 +10,3 @@
     $app->run();
 
     // [END gae_php_app_bootstrap]
-
-    // use Psr\Http\Message\ResponseInterface as Response;
-    // use Psr\Http\Message\ServerRequestInterface as Request;
-    // use Slim\Factory\AppFactory;
-
-    // $app = AppFactory::create();
-    // $app->addRoutingMiddleware();
-    // $app->addErrorMiddleware(true, true, true);
-    // $app->setBasePath("/index.php");
-
-    
-
-    // $app->run();
--- public/app.php
+++ public/app.php
@@ -1,21 +1,12 @@
 <?php
 
     /**
-     * Create a new Slim PHP Application with Twig.  Configure it for debugging.
+     * Create a new Slim PHP Application.  Configure it for debugging.
      */
-    use DI\Container;
-    use Google\Cloud\Samples\AppEngine\GettingStarted\CloudSqlDataModel;
-    // [START gae_php_app_storage_client_import]
-    use Google\Cloud\Storage\StorageClient;
-
-    // [END gae_php_app_storage_client_import]
     use Slim\Factory\AppFactory;
-    // use Slim\Views\Twig;
-
-    // AppFactory::setContainer($container = new Container());
 
     $app = AppFactory::create();
-
+    $app->addRoutingMiddleware();
     // Display errors
     $app->addErrorMiddleware(true, true, true);
 
