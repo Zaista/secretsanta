f9c62533f6201102c73bb5b8e4e17f02d5d844cc
Zaista <ilicjovan89@gmail.com>
2023-03-13 12:15:00 +0100

    fix: update history name chage

--- utils/historyPipeline.js
+++ utils/historyPipeline.js
@@ -1,100 +1,81 @@
 import { getClient } from './database.js';
 
 export async function getHistory() {
-  const stage1 = {
-    $unwind: {
-      path: '$gifts'
-    }
-  };
-
-  // match santaId and userId to get santa info
-  const stage2 = {
-    $lookup: {
-      from: 'users',
-      localField: 'gifts.santaId',
-      foreignField: 'userId',
-      as: 'santaUser'
-    }
-  };
-
-  // match childId and userId to get child info
-  const stage3 = {
-    $lookup: {
-      from: 'users',
-      localField: 'gifts.childId',
-      foreignField: 'userId',
-      as: 'childUser'
-    }
-  };
-
-  const stage4 = {
-    $unwind: {
-      path: '$santaUser'
-    }
-  };
-
-  const stage5 = {
-    $unwind: {
-      path: '$childUser'
-    }
-  };
-
-  // filter only necessary fields
-  const stage6 = {
-    $project: {
-      year: 1,
-      location: 1,
-      location_image: 1,
-      gift: '$gifts.gift',
-      gift_image: '$gifts.gift_image',
-      santa: '$santaUser.firstName',
-      child: '$childUser.firstName'
-    }
-  };
-
-  const stage7 = {
-    $group: {
-      _id: '$year',
-      year: {
-        $first: '$$ROOT.year'
-      },
-      location: {
-        $first: '$$ROOT.location'
-      },
-      location_image: {
-        $first: '$$ROOT.location_image'
-      },
-      gifts: {
-        $push: {
-          santa: '$$ROOT.santa',
-          child: '$$ROOT.child',
-          gift: '$$ROOT.gift',
-          gift_image: '$$ROOT.gift_image'
+  const pipeline = [
+    {
+      $unwind: {
+        path: '$gifts'
+      }
+    },
+    {
+      // match santaId and userId to get santa info
+      $lookup: {
+        from: 'users',
+        localField: 'gifts.santaId',
+        foreignField: 'userId',
+        as: 'santaUser'
+      }
+    },
+    {
+      // match childId and userId to get child info
+      $lookup: {
+        from: 'users',
+        localField: 'gifts.childId',
+        foreignField: 'userId',
+        as: 'childUser'
+      }
+    },
+    {
+      $unwind: {
+        path: '$santaUser'
+      }
+    },
+    {
+      $unwind: {
+        path: '$childUser'
+      }
+    },
+    {
+      // filter only necessary fields
+      $project: {
+        year: 1,
+        location: 1,
+        location_image: 1,
+        gift: '$gifts.gift',
+        gift_image: '$gifts.gift_image',
+        santa: '$santaUser.name',
+        child: '$childUser.name'
+      }
+    },
+    {
+      $group: {
+        _id: '$year',
+        year: {
+          $first: '$$ROOT.year'
+        },
+        location: {
+          $first: '$$ROOT.location'
+        },
+        location_image: {
+          $first: '$$ROOT.location_image'
+        },
+        gifts: {
+          $push: {
+            santa: '$$ROOT.santa',
+            child: '$$ROOT.child',
+            gift: '$$ROOT.gift',
+            gift_image: '$$ROOT.gift_image'
+          }
         }
       }
+    },
+    {
+      $project: { _id: 0 }
+    },
+    {
+      $sort: { year: -1 }
     }
-  };
-
-  const stage8 = {
-    $project: { _id: 0 }
-  };
-
-  const stage9 = {
-    $sort: { year: -1 }
-  };
-
-  const pipeline = [];
-  pipeline.push(
-    stage1,
-    stage2,
-    stage3,
-    stage4,
-    stage5,
-    stage6,
-    stage7,
-    stage8,
-    stage9
-  );
+  ];
 
   const client = await getClient();
 
