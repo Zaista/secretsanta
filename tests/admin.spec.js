8592810bcb8879a83411bf961f0dc524245d5799
Jovan <jovan.ilic@cloudflight.io>
2023-12-27 01:45:25 +0100

    fix: lint

--- tests/admin.spec.js
+++ tests/admin.spec.js
@@ -1,34 +1,32 @@
 // @ts-check
-import {test, expect} from '@playwright/test';
-import {faker} from '@faker-js/faker';
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 import { login, registerUser } from './helpers/login.js';
 import { createGroup, addUserToGroup } from './helpers/admin.js';
 
 test.describe('admin tests', () => {
+  test('admin can draft pairs', async ({ page }) => {
+    const adminUser = {
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+    await registerUser(page.request, adminUser.email, adminUser.password);
+    await login(page.request, adminUser.email, adminUser.password);
+    const user1 = {
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+    const user2 = {
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+    await createGroup(page.request, faker.word.noun());
+    await addUserToGroup(page.request, user1.email);
+    await addUserToGroup(page.request, user2.email);
 
-    test('admin can draft pairs', async ({ page}) => {
-        
-        const adminUser = {
-            email: faker.internet.email(),
-            password: faker.internet.password()
-        }
-        await registerUser(page.request, adminUser.email, adminUser.password);
-        await login(page.request, adminUser.email, adminUser.password);
-        const user1 = {
-            email: faker.internet.email(),
-            password: faker.internet.password()
-        }
-        const user2 = {
-            email: faker.internet.email(),
-            password: faker.internet.password()
-        }
-        await createGroup(page.request, faker.word.noun());
-        await addUserToGroup(page.request, user1.email);
-        await addUserToGroup(page.request, user2.email);
-        
-        await page.goto('/admin');
+    await page.goto('/admin');
 
-        await page.getByRole('button', {name: 'Draft'}).click();
-        await expect(page.locator('#footerAlert')).toHaveText('Pairs successfully drafted');
-    });
+    await page.getByRole('button', { name: 'Draft' }).click();
+    await expect(page.locator('#footerAlert')).toHaveText('Pairs successfully drafted');
+  });
 });
--- tests/helpers/admin.js
+++ tests/helpers/admin.js
@@ -1,11 +1,11 @@
 export async function createGroup(request, name) {
-    await request.post('admin/api/group/create', {
-        form: { groupName: name }
-    });
+  await request.post('admin/api/group/create', {
+    form: { groupName: name }
+  });
 }
 
 export async function addUserToGroup(request, email) {
-    await request.post('admin/api/user', {
-        form: { email: email }
-    });
+  await request.post('admin/api/user', {
+    form: { email }
+  });
 }
