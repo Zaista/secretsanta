85b59bb3fd59f2a1a02a1fa5936f79cdbb7d6c2f
Jovan Ilić <jovan.ilic@cloudflight.io>
2024-03-03 23:55:37 +0100

    fix: lint 2

--- tests/admin.spec.js
+++ tests/admin.spec.js
@@ -11,7 +11,7 @@ test.describe('admin tests', () => {
       const groupData = await createDraftedGroup(page.request);
       await login(page.request, groupData.users.admin.email, groupData.users.admin.password);
       await page.goto('/admin');
-      
+
       await expect(page.locator('#groupName')).toHaveText(groupData.group.name);
       const updatedName = faker.word.noun();
       await page.getByLabel('Group name').fill(updatedName);
