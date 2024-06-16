9f14f73aa89d38c6bcba864d9757c8ca9dcd64a2
Jovan Ilić <ilicjovan89@gmail.com>
2024-06-16 19:07:56 +0200

    Improve testing chat

--- tests/chat.spec.js
+++ tests/chat.spec.js
@@ -3,19 +3,24 @@ import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import { login } from './helpers/login.js';
 import { sendMessage } from './helpers/chat.js';
-import { createDraftedGroup } from './helpers/setup.js';
-import { updateGroup } from './helpers/admin.js';
+import { createNewGroup } from './helpers/setup.js';
 
 test.describe('chat tests', () => {
+  let groupData;
+  let page;
+
+  test.beforeAll('setup', async ({ browser })=> {
+    page = await browser.newPage();
+    groupData = await createNewGroup(page.request);
+  })
+
   test('user can send a message', async ({ page }) => {
-    const groupData = await createDraftedGroup(page.request);
     await login(
       page.request,
       groupData.users.user1.email,
       groupData.users.user1.password
     );
     await page.goto('/chat');
-
     await expect(page).toHaveTitle('Secret Santa Chat');
 
     await page.getByPlaceholder('Your message').fill(faker.word.words());
@@ -28,15 +33,7 @@ test.describe('chat tests', () => {
     await expect(page.getByText('Just now...')).toBeVisible();
   });
 
-  test('user should receive a chat email', async ({ page }) => {
-    const groupData = await createDraftedGroup(page.request);
-    const updatedGroupData = {
-      name: groupData.group.name,
-      userAddedNotification: false,
-      messageSentNotification: true,
-      yearDraftedNotification: false,
-    };
-    await updateGroup(page.request, updatedGroupData);
+  test('user can see received messages', async ({ page }) => {
     await login(
       page.request,
       groupData.users.user1.email,
@@ -47,34 +44,17 @@ test.describe('chat tests', () => {
       email: groupData.users.user2.email,
       message: faker.word.words(10),
     };
-    const messageData = await sendMessage(page.request, message);
-    await page.goto(messageData.emailUrl);
+    await sendMessage(page.request, message);
 
-    await expect(page.locator('#message-header')).toContainText(
-      'Secret Santa Question'
-    );
-    await expect(page.locator('#message-header')).toContainText(
-      '<secretsanta@jovanilic.com>'
-    );
-    await expect(page.locator('#message-header')).toContainText(message.email);
-    await expect(
-      page.frameLocator('#message iframe').locator('body')
-    ).toContainText(message.message);
-  });
-
-  test('user should not receives a chat email', async ({ page }) => {
-    const groupData = await createDraftedGroup(page.request);
     await login(
       page.request,
       groupData.users.user2.email,
       groupData.users.user2.password
     );
-    const message = {
-      userId: groupData.users.user1.id,
-      email: groupData.users.user1.email,
-      message: faker.word.words(10),
-    };
-    const messageData = await sendMessage(page.request, message);
-    await expect(messageData).not.toHaveProperty('emailUrl');
+    await page.goto('/chat');
+
+    await expect(page.locator('[data-name="chatTo"]')).toContainText(groupData.users.user2.name);
+    await expect(page.locator('[data-name="chatMessage"]')).toHaveText(message.message);
+    await expect(page.locator('[data-name="chatFrom"]')).toHaveText('From: Anonymous');
   });
 });
